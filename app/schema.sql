8b9968b43faa559bb670a6279fbdc23b805e5245
Neil Castelino <neil.fl93@gmail.com>
2019-02-14 19:06:47 -0500

    update schema to add in a users table

--- app/schema.sql
+++ app/schema.sql
@@ -1,6 +1,13 @@
 drop table if exists servers;
+drop table if exists users;
 
 create table servers (
-    name varchar(50),
-    id varchar(100) primary key
+    name text,
+    id text primary key
+);
+
+create table users (
+    id integer primary key autoincrement,
+    username text unique not null,
+    password text not null
 );
