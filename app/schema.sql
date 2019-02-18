1217ddf6756e1df577abfcfd5422d2f1a2427d0f
Neil Castelino <neil.fl93@gmail.com>
2019-02-18 17:22:23 -0500

    update schema

--- app/schema.sql
+++ app/schema.sql
@@ -2,8 +2,20 @@ drop table if exists servers;
 drop table if exists users;
 
 create table servers (
-    name text,
+    name text not null,
     id text primary key
+);
+
+create table services (
+    name text not null,
+    id text primary key
+);
+
+create table logged_times (
+    id integer primary key autoincrement,
+    server_name text,
+    service_name text,
+    logged_at datetime default current_timestamp not null
 );
 
 create table users (
